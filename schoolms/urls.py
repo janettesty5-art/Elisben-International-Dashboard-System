8461a7f57f4916aecc5fe486c1970960a9019476
janettesty5-art <janettesty5@gmail.com>
2026-03-12 22:55:18 +0100

    Fix  stamp th

--- schoolms/urls.py
+++ schoolms/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('django-admin/', admin.site.urls),  # Changed from 'admin/' to 'django-admin/'
     path('', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
