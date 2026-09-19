8a54bf295c052792584d29b34b82b37a19a2da45
janettesty5-art <janettesty5@gmail.com>
2026-09-19 16:56:43 +0100

    CM===--9293

--- schoolms/settings.py
+++ schoolms/settings.py
@@ -190,5 +190,4 @@ CLOUDINARY_STORAGE = {
     'API_KEY': config('CLOUDINARY_API_KEY', default=''),
     'API_SECRET': config('CLOUDINARY_API_SECRET', default=''),
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
